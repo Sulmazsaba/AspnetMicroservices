1447eb757cd3cd7711ecabbbbaff2370a4805b58
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2022-03-20 23:08:32 +0330

    added migrations

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,6 +1,15 @@
+using Ordering.Api.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApplicationService();
+builder.Services.AddInfrastructureService(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,4 +29,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContextSeed>>();
+    OrderContextSeed.SeedAsync(context, logger).Wait();
+});
 app.Run();
