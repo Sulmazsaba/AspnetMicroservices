7d912d79dbd33595d47bcd8398140aadd86a0242
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2022-04-09 01:16:08 +0430

    added discount.grpc

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,7 +1,10 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IDiscountRepository,DiscountRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,6 +12,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
