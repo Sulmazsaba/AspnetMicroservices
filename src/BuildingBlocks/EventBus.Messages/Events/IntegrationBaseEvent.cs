cf30eb711ec0e0ed2b49c1e525cff1071c8072b0
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2022-04-11 13:21:25 +0430

    added Event Bus

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+
+        }
+
+        public Guid Id { get; init; }
+        public DateTime CreationDate { get; init; }
+
+
+
+
+    }
+}
